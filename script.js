5fdb23bdc70db9d885473afeeef8bcc4bcaee7ab
Inna Mon <inna.mon@gmail.com>
2019-04-19 09:44:55 -0500

    added js code to change the active class of the current displayed nav-item

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log("hello");
 
+// Get nav-bar to change from transparent backgrounds upon scrolling past hero image 
 $(window).scroll(function() {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 680);
 });
+
+// Get the container element
+const ul = document.getElementById("navbarSupportedContent");
+
+// Get all nav-items inside the container
+const navItem = ul.getElementsByClassName("nav-item");
+
+// Loop through the nav-items and add the active class to the current/clicked item
+for (let i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener("click", function() {
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
