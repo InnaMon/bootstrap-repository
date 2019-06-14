4091bca7892a5238330569860045d13f85393980
Inna Mon <inna.mon@gmail.com>
2019-06-14 17:04:01 -0500

    copied code from ModalOne and added to ModalTwo

--- script.js
+++ script.js
@@ -31,30 +31,59 @@ $('.carousel').carousel({
     interval: 3000
 })
 
-// MODAL CODE
+// FIRST MODAL CODE
 // Get the modal
-var modal = document.getElementById("myModal");
+var firstModal = document.getElementById("firstModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("firstBtn");
+var firstBtn = document.getElementById("firstBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanOne = document.getElementsByClassName("closeOne")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function(e) {
-  modal.style.display = "block";
+firstBtn.onclick = function(e) {
+    firstModal.style.display = "block";
   e.preventDefault();
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanOne.onclick = function() {
+    firstModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == firstModal) {
+    firstModal.style.display = "none";
   }
 }
+
+// SECOND MODAL CODE
+var secondModal = document.getElementById("secondModal");
+
+// Get the button that opens the modal
+var secondBtn = document.getElementById("secondBtn");
+
+// Get the <span> element that closes the modal
+var spanTwo = document.getElementsByClassName("closeTwo")[0];
+
+// When the user clicks on the button, open the modal 
+secondBtn.onclick = function(e) {
+    secondModal.style.display = "block";
+  e.preventDefault();
+}
+
+// When the user clicks on <span> (x), close the modal
+spanTwo.onclick = function() {
+    secondModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == secondModal) {
+    secondModal.style.display = "none";
+  }
+}
+
+// THIRD MODAL CODE
