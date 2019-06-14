88c64f4ad1c2981b647f4e01d23f57f6e950ae8a
Inna Mon <inna.mon@gmail.com>
2019-06-14 12:19:49 -0500

    copy and pasted modal code from w3schools and added javascript code to prevent window from jumping back to the very top upon modal showing up

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ for (let i = 0; i < navItem.length; i++) {
 $('.carousel').carousel({
     interval: 3000
 })
+
+// MODAL CODE
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("firstBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
