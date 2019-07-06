7c8aebe8d62274aaeece010f57f70f34d3ded21d
Inna Mon <inna.mon@gmail.com>
2019-07-06 13:42:35 -0500

    added js code that collapses navbar hamburger as soon as select nav item

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ for (let i = 0; i < navItem.length; i++) {
   });
 }
 
+//COLLAPSE HAMBURGER NAVBAR IN MOBILE/TABLET MODE WHEN SELECT NAV-ITEM
+
+$(document).ready(function () {
+    $(".navbar-nav a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+  });
+
 //CAROUSEL FADE IN INTERVAL
 
 $('.carousel').carousel({
